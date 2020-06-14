d582a995c5a24cb2e68dcedf01df22b45198066d
shitzkhn <shitzkhn@gmail.com>
2020-06-14 23:28:04 +0530

    A** Algo Done

--- Maze.js
+++ Maze.js
@@ -1,11 +1,13 @@
-var size = 5;
+var size = 50;
 var rows = size;
 var cols = size;
 var start = undefined;
 var end = undefined;
 var grid = new Array(rows);
-var openSet = [];
-var closedSet = [];
+var openSet = new Set([]);
+var closedSet = new Set([]);
+var path = [];
+var done = false;
 
 function Dot(i, j) {
   this.x = i;
@@ -16,7 +18,9 @@ function Dot(i, j) {
   this.obstacle = false;
   this.cop = false;
   this.neighbors = [];
-  if (random(1) < 0.3) {
+  this.parent = undefined;
+
+  if (random(1) < 0.4) {
     this.obstacle = true;
   }
   if (random(1) < 0.008) {
@@ -86,8 +90,51 @@ function Dot(i, j) {
   };
 }
 
+function heuristic(a, b) {
+  return dist(a.x, a.y, b.x, b.y);
+}
 function pathFinder() {
-  // Loop through openSet
+  while (openSet.size != 0) {
+    var current = undefined;
+    for (val of openSet) {
+      if (current === undefined) {
+        current = val;
+      } else {
+        if (val.f < current.f) {
+          current = val;
+        }
+      }
+    }
+
+    if (current === end) {
+      done = true;
+      console.log("DONE!");
+      return;
+    }
+    openSet.delete(current);
+    closedSet.add(current);
+    var neighbors = current.neighbors;
+
+    for (val of neighbors) {
+      if (closedSet.has(val)) {
+        continue;
+      }
+      var tempg = current.g + 1;
+
+      if (openSet.has(val)) {
+        if (tempg < val.g) {
+          val.g = tempg;
+        }
+      } else {
+        val.g = tempg;
+        openSet.add(val);
+      }
+
+      val.h = heuristic(val, end);
+      val.f = val.h + val.g;
+      val.parent = current;
+    }
+  }
 }
 function reset() {}
 
@@ -118,8 +165,19 @@ function setup() {
   start.obstacle = false;
   end.obstacle = false;
 
-  openSet.push(start);
+  openSet.add(start);
   pathFinder();
+
+  if (!done) {
+    console.log("No Solution");
+  } else {
+    var temp = end;
+    while (temp != undefined) {
+      path.push(temp);
+      temp = temp.parent;
+    }
+  }
+
   console.log(grid);
   //   console.log("Setup");
 }
@@ -140,4 +198,16 @@ function draw() {
   for (val of closedSet) {
     val.show(color(0, 255, 0));
   }
+  for (val of path) {
+    val.show(color(0, 0, 255));
+  }
+
+  noFill();
+  stroke(color(0, 120, 120));
+  beginShape();
+  strokeWeight(w / 3);
+  for (var i = 0; i < path.length; ++i) {
+    vertex(path[i].i * w + w / 2, path[i].j * h + h / 2);
+  }
+  endShape();
 }
